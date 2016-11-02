3d86ee62dc7c3d0d0d25cb1ffd3c93eeefa9be52
Chrissweir <christopher.weir@hotmail.com>
2016-11-02 18:22:50 +0000

    Changes

--- webapp.go
+++ webapp.go
@@ -3,20 +3,18 @@ package main
 import (
 	"gopkg.in/macaron.v1"
 	"gopkg.in/mgo.v2"
-    "fmt"
-    "io"
-    "net/http"
-    "os"
+	"net/http"
+	"fmt"
+	"io/ioutil"
 )
 
 func main() {
 	m := macaron.Classic()
 	m.Use(macaron.Renderer())
-	m.Get("/upload", uploadHandler)
+	m.Get("/upload", uploadhandler)
 	m.Run()
 }
-
-func connect() {
+func uploadhandler(w http.ResponseWriter, req *http.Request) {
 	session, err := mgo.Dial("127.0.0.1:27017")
 	if err != nil {
 		panic(err)
@@ -24,36 +22,48 @@ func connect() {
 
 	defer session.Close()
 
-	session.SetMode(mgo.Monotonic, true)
+	// Capture multipart form file information
+	file, handler, err := req.FormFile("uploadfile")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Read the file into memory
+	data, err := ioutil.ReadAll(file)
+	// ... check err value for nil
+
+	// Specify the Mongodb database
+	my_db := session.DB("Images")
+	filename := handler.Filename
+
+	// Create the file in the Mongodb Gridfs instance
+	my_file, err := my_db.GridFS("fs").Create(filename)
+	// ... check err value for nil
+
+	// Write the file to the Mongodb Gridfs instance
+	n, err := my_file.Write(data)
+	// ... check err value for nil
+
+	// Close the file
+	err = my_file.Close()
+	// ... check err value for nil
+
+	// Write a log type message
+	fmt.Printf("%d bytes written to the Mongodb instance\n", n)
+
+	// ... other statements redirecting to rest of user flow...
 }
+ /*func uploadhandler(r *http.Request){
 
+	 session, err := mgo.Dial("127.0.0.1:27017")
+	 if err != nil {
+		 panic(err)
+	 }
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	 defer session.Close()
+	 db := session.DB("Images")
 
- 	// the FormFile function takes in the POST input id file
- 	file, header, err := r.FormFile("uploadfile")
+	 file, header, err:= r.FormFile("uploadfile") // the input file by form
+	 defer file.Close()
 
- 	if err != nil {
- 		fmt.Fprintln(w, err)
- 		return
- 	}
-
- 	defer file.Close()
-
- 	out, err := os.Create("/tmp/uploadedfile")
- 	if err != nil {
- 		fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
- 		return
- 	}
-
- 	defer out.Close()
-
- 	// write the content from POST to the file
- 	_, err = io.Copy(out, file)
- 	if err != nil {
- 		fmt.Fprintln(w, err)
- 	}
-
- 	fmt.Fprintf(w, "File uploaded successfully : ")
- 	fmt.Fprintf(w, header.Filename)
- }
+ }*/
